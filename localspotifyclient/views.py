1dd027a8c2b0ea5ee74fdd3358bb3d81b3d79891
Joe Conyers III <joe@joeconyers.com>
2011-10-16 18:04:09 -0400

    Add support for current track time

--- localspotifyclient/views.py
+++ localspotifyclient/views.py
@@ -4,22 +4,33 @@ import sys
 from appscript import *
 from spotipy import Spotipy
 from django.http import HttpResponse
+import datetime
 import os
 
 spotify = Spotipy()
 
 def home(request):
+	
 	try:
 		track_name = app(u'Spotify').current_track.name.get()
 	except:
 		pass
 		track_name = ''	
+		
 	try:	
 	  artist_name = app(u'Spotify').current_track.artist.get()
 	except:
 	  pass
 	  artist_name = ''
-	return render_to_response('localspotifyclient/index.html', {'TrackName': track_name, 'ArtistName' : artist_name })
+	
+	try:
+		track_time = app(u'Spotify').player_position.get()
+		track_time = "%02d:%02d" % divmod(track_time, 60)
+	except:
+		pass
+		track_time = '0:00'
+	
+	return render_to_response('localspotifyclient/index.html', {'TrackName': track_name, 'ArtistName' : artist_name, 'TrackTime' : track_time })
 	
 def playpause(request):
 	spotify.play_pause()
